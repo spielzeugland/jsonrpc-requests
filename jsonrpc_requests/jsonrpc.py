e273423b095ffbaa3fef0ac12fb6d9d2cdfc65cd
spielzeugland <spielzeugland@users.noreply.github.com>
2016-06-02 21:51:43 +0200

    Allow reusing JSON RPC proxy for direct calls to Kodi API

--- jsonrpc_requests/jsonrpc.py
+++ jsonrpc_requests/jsonrpc.py
@@ -2,54 +2,24 @@ import random
 import sys
 import json
 import functools
-
 import requests
-
 
 class JSONRPCError(Exception):
     """Root exception for all errors related to this library"""
 
-
 class TransportError(JSONRPCError):
     """An error occurred while performing a connection to the server"""
-
 
 class ProtocolError(JSONRPCError):
     """An error occurred while dealing with the JSON-RPC protocol"""
 
-
-class Server(object):
-    """A connection to a HTTP JSON-RPC server, backed by requests"""
-
-    def __init__(self, url, session=None, **requests_kwargs):
-        self.session = session or requests.Session()
-        self.session.headers.update({
-            'Content-Type': 'application/json',
-            'Accept': 'application/json-rpc',
-        })
-        self.request = functools.partial(self.session.post, url, **requests_kwargs)
+class Proxy(object):
 
     def send_request(self, method_name, is_notification, params):
-        """Issue the HTTP request to the server and return the method result (if not a notification)"""
-        request_body = self.serialize(method_name, params, is_notification)
-        try:
-            response = self.request(data=request_body)
-        except requests.RequestException as requests_exception:
-            raise TransportError('Error calling method %r' % method_name, requests_exception)
+        ''''Will be overiden by subclasses'''''
+        pass
 
-        if response.status_code != requests.codes.ok:
-            raise TransportError(response.status_code)
-
-        if not is_notification:
-            try:
-                parsed = response.json()
-            except ValueError as value_error:
-                raise TransportError('Cannot deserialize response body', value_error)
-
-            return self.parse_result(parsed)
-
-    @staticmethod
-    def parse_result(result):
+    def deserialize(self, result):
         """Parse the data returned by the server according to the JSON-RPC spec. Try to be liberal in what we accept."""
         if not isinstance(result, dict):
             raise ProtocolError('Response is not a dictionary')
@@ -87,6 +57,53 @@ class Server(object):
             raise ProtocolError('JSON-RPC spec forbids mixing arguments and keyword arguments')
         return self.send_request(method_name, is_notification, args or kwargs)
 
+class Server(Proxy):
+    """A connection to a HTTP JSON-RPC server, backed by requests"""
+
+    def __init__(self, url, session=None, **requests_kwargs):
+        self.session = session or requests.Session()
+        self.session.headers.update({
+            'Content-Type': 'application/json',
+            'Accept': 'application/json-rpc',
+        })
+        self.request = functools.partial(self.session.post, url, **requests_kwargs)
+
+    def send_request(self, method_name, is_notification, params):
+        """Issue the HTTP request to the server and return the method result (if not a notification)"""
+        request_body = self.serialize(method_name, params, is_notification)
+        try:
+            response = self.request(data=request_body)
+        except requests.RequestException as requests_exception:
+            raise TransportError('Error calling method %r' % method_name, requests_exception)
+
+        if response.status_code != requests.codes.ok:
+            raise TransportError(response.status_code)
+
+        if not is_notification:
+            try:
+                parsed = response.json()
+            except ValueError as value_error:
+                raise TransportError('Cannot deserialize response body', value_error)
+
+            return self.deserialize(parsed)
+
+class Local(Proxy):
+
+    def __init__(self, xbmc):
+        self._xbmc = xbmc
+
+    def send_request(self, method_name, is_notification, params):
+        if is_notification:
+            raise ProtocolError('Kodi does not support notifications for local JSON-RPC calls')
+
+        request_body = self.serialize(method_name, params, is_notification)
+        try:
+            response = self._xbmc.executeJSONRPC(request)
+        except Exception as requests_exception:
+            raise TransportError('Error calling method %r' % method_name, requests_exception)
+
+        parsed = json.loads(response)
+        return self.deserialize(parsed)
 
 class Method(object):
     def __init__(self, request_method, method_name):
@@ -102,3 +119,4 @@ class Method(object):
 
     def __call__(self, *args, **kwargs):
         return self.__request_method(self.__method_name, args, kwargs)
+
